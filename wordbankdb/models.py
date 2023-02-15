7b154dcc89abc86ebb17ac8f7618564a516a6b0a
Charles Jarquio <cjarquio@gmail.com>
2023-02-15 12:10:15 -0800

    Connect ilocano project to postgres

--- wordbankdb/models.py
+++ wordbankdb/models.py
@@ -1,3 +1,10 @@
+"""Module prvoding function to define db model"""
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    """A model for Ilocano word with pronunciation and English translation"""
+    word = models.CharField(max_length=20)
+    pronunciation = models.CharField(max_length=30)
+    translation = models.CharField(max_length=20)
+    
