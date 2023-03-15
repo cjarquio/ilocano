a9ad1896c0839f1ff2e6491b81802ea381807123
Charles Jarquio <cjarquio@gmail.com>
2023-03-14 17:12:04 -0700

    Reorg word bank list

--- ilocano-backend/wordbankdb/models.py
+++ ilocano-backend/wordbankdb/models.py
@@ -5,12 +5,12 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class WordBank(models.Model):
     """A model for Ilocano word with pronunciation and English translation"""
-    category = models.CharField(max_length=20 , null=True, blank=True)
-    english = models.CharField(max_length=30)
     ilokano = models.CharField(max_length=20)
+    english = models.CharField(max_length=30)
+    category = models.CharField(max_length=20 , null=True, blank=True)
     variations = ArrayField(models.CharField(max_length=20), null=True, blank=True)
     lesson_number = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
 
     def __str__(self) -> str:
-        return str(self.ilokano)
+        return f"{self.ilokano} {self.english}"
     
--- ilocano-backend/wordbankdb/serializers.py
+++ ilocano-backend/wordbankdb/serializers.py
@@ -7,4 +7,4 @@ class WordBankSerializer(serializers.ModelSerializer):
     class Meta:
         """Information from word bank to be converted"""
         model = WordBank
-        fields = ('id', 'category', 'english', 'ilokano', 'variations', 'lesson_number')
+        fields = ('id', 'ilokano', 'english', 'category', 'variations', 'lesson_number')
