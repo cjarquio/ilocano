bb80d6c81477013fa205904ac4f577999ca0c4dc
Charles Jarquio <cjarquio@gmail.com>
2023-05-12 07:17:20 -0700

    Add ability to provide free translation and submit

--- lets-speak-ilocano/src/components/Sections/TranslatingDialog/TranslatingDialogSection.tsx
+++ lets-speak-ilocano/src/components/Sections/TranslatingDialog/TranslatingDialogSection.tsx
@@ -1,4 +1,5 @@
-import {Box, Input, Text} from '@chakra-ui/react'
+import {Box, Button, FormControl, Input, Text} from '@chakra-ui/react'
+import { useState } from 'react'
 
 interface DialogSectionProps {
   dialog: string[]
@@ -6,16 +7,35 @@ interface DialogSectionProps {
 
 export const TranslatingDialogSection: React.FC<DialogSectionProps> = (props: DialogSectionProps) => {
   const {dialog} = props
+
+  const [studentResponses, setStudentResponses] = useState<string[]>(dialog.map(() => ''))
+
+  const handleInputChange = (value: string, index: number) => {
+    const newInput = [...studentResponses]
+    newInput[index] = value
+    setStudentResponses(newInput)
+  }
+
   return (
-    <Box>
-      {
-        dialog.map(d => (
-          <Box display={'flex'} alignItems={'center'} padding={'1rem 0'} key={d}>
-            <Text paddingRight={'0.5rem'}>{d.substring(0, 2)}</Text>
-            <Input variant='flushed' />
-          </Box>
-        ))
-      }
-    </Box>
+    <FormControl>
+      <Box>
+        {
+          dialog.map((d, index) => (
+            <Box display={'flex'} alignItems={'center'} padding={'1rem 0'} key={d}>
+              <Text paddingRight={'0.5rem'}>{d.substring(0, 2)}</Text>
+              <Input variant='flushed' value={studentResponses[index]} onChange={(e) => handleInputChange(e.target.value, index)} />
+            </Box>
+          ))
+        }
+      </Box>
+      <Button
+        mt={4}
+        colorScheme='teal'
+        type='submit'
+        onClick={()=>console.log('Here is the input', studentResponses)}
+      >
+        Submit
+      </Button>
+    </FormControl>
   )
 }
