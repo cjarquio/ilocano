80928871a4c15bf6033b1c3407292b7508016820
Charles Jarquio <cjarquio@gmail.com>
2024-05-15 17:43:13 -0700

    Create log in page

---
+++ lets-speak-ilocano/src/Components/Pages/Login/Login.tsx
@@ -0,0 +1,60 @@
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+  Divider,
+} from "@mantine/core";
+import { GoogleButton } from "../../Icons/GoogleButton";
+import classes from "./Login.module.css";
+
+export function Login() {
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center" className={classes.title}>
+        Welcome back!
+      </Title>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <Group grow mb="md" mt="md">
+          <GoogleButton radius="xl">Google</GoogleButton>
+        </Group>
+
+        <Divider
+          label="Or continue with email"
+          labelPosition="center"
+          my="lg"
+        />
+        <TextInput label="Email" placeholder="you@mantine.dev" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group justify="space-between" mt="lg">
+          <Checkbox label="Remember me" />
+          <Anchor component="button" size="sm">
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+          Do not have an account yet?{" "}
+          <Anchor size="sm" component="button">
+            Create account
+          </Anchor>
+        </Text>
+      </Paper>
+    </Container>
+  );
+}
