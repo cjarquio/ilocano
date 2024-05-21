e005523f4ac1dfd9c63d15815c75b0958445be43
Charles Jarquio <cjarquio@gmail.com>
2024-05-20 23:02:34 -0700

    Add ability to log in

--- lets-speak-ilocano/src/Components/Pages/Login/Login.tsx
+++ lets-speak-ilocano/src/Components/Pages/Login/Login.tsx
@@ -11,10 +11,33 @@ import {
   Button,
   Divider,
 } from "@mantine/core";
+import axios from "axios";
 import { GoogleButton } from "../../Icons/GoogleButton";
 import classes from "./Login.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    axios
+      .post(`${import.meta.env.VITE_DJANGO_API_URL}/login/`, {
+        username: username,
+        password: password,
+      })
+      .then(() => console.log("login successful"))
+      .catch((err) => console.error(err));
+  };
+  const onUsernameChange = (username: string) => {
+    setUsername(username);
+  };
+  const onPasswordChange = (password: string) => {
+    setPassword(password);
+  };
+
   return (
     <Container size={420} my={40}>
       <Title ta="center" className={classes.title}>
@@ -31,10 +54,16 @@ export default function Login() {
           labelPosition="center"
           my="lg"
         />
-        <TextInput label="Email" placeholder="Email" required />
+        <TextInput
+          label="Username"
+          placeholder="Username"
+          onChange={(e) => onUsernameChange(e.target.value)}
+          required
+        />
         <PasswordInput
           label="Password"
           placeholder="Your password"
+          onChange={(e) => onPasswordChange(e.target.value)}
           required
           mt="md"
         />
@@ -44,13 +73,17 @@ export default function Login() {
             Forgot password?
           </Anchor>
         </Group>
-        <Button fullWidth color="teal" mt="xl">
+        <Button fullWidth color="teal" mt="xl" onClick={handleLogin}>
           Sign in
         </Button>
 
         <Text c="dimmed" size="sm" ta="center" mt={5}>
           Do not have an account yet?{" "}
-          <Anchor size="sm" component="button">
+          <Anchor
+            size="sm"
+            component="button"
+            onClick={() => navigate("/register")}
+          >
             Create account
           </Anchor>
         </Text>
