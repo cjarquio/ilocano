8ad0e78f1e8454aa8c7a8296fdb4d9644fe0d3fb
Charles Jarquio <cjarquio@gmail.com>
2025-06-25 12:12:06 -0700

    Create admin model

--- ilocanobackend/ilocano/admin.py
+++ ilocanobackend/ilocano/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from ilocano.models import Word
 # Register your models here.
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('ilocano', 'english', 'part_of_speech', 'example_sentence')
+    search_fields = ('ilocano', 'english', 'part_of_speech')
+    
+admin.site.register(Word, WordAdmin)
