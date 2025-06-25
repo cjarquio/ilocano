30bbdedae6caf43fa2ddaf3f1049d5223d241553
Charles Jarquio <cjarquio@gmail.com>
2025-06-25 12:09:45 -0700

    Connect django to postgres; set up word model

--- ilocanobackend/ilocano/models.py
+++ ilocanobackend/ilocano/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    ilocano = models.CharField(max_length=255, unique=True)
+    english = models.TextField()
+    part_of_speech = models.CharField(max_length=50, blank=True, null=True)
+    example_sentence = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.ilocano} - {self.english}'
