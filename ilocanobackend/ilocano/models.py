9f17348a3de68b1917b0b0220115d39c4ea36efe
Charles Jarquio <cjarquio@gmail.com>
2025-06-25 14:56:23 -0700

    Update model to include possible lesson number

--- ilocanobackend/ilocano/models.py
+++ ilocanobackend/ilocano/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Word(models.Model):
     ilocano = models.CharField(max_length=255, unique=True)
     english = models.TextField()
+    lesson_number = models.IntegerField(blank=True, null=True)
     part_of_speech = models.CharField(max_length=50, blank=True, null=True)
     example_sentence = models.TextField(blank=True, null=True)
 
